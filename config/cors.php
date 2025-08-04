d98a858fb257e760511159bd11152a427a054833
mwbsmith <mwbsmith@yahoo.com>
2025-08-03 19:59:32 -0600

    changed cors to allow api access to all

--- config/cors.php
+++ config/cors.php
@@ -1,13 +1,13 @@
 <?php
 // config/cors.php (Laravel 12 native CORS)
-return [
+// return [
     /*
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
     |--------------------------------------------------------------------------
     */
 
-    'paths' => [
+ /*   'paths' => [
         'api/*',
         'sanctum/csrf-cookie',
     ],
@@ -28,4 +28,26 @@ return [
     'max_age' => 0,
 
     'supports_credentials' => true,
+]; */
+
+return [
+
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+    ],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['*'], // Allow all origins
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false, // MUST be false when using '*'
 ];
